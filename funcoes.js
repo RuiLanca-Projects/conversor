601cef1d68c1a12ccaedd5b3281c857fdac695b6
RuiLanca-Projects <129678863+RuiLanca-Projects@users.noreply.github.com>
2023-11-28 15:13:38 +0000

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,26 @@
+function tecla(caracter){
+  let ecra = document.querySelector(".ecra");
+
+  ecra.innerHTML += caracter;
+
+  converter();
+}
+
+
+function limpar(){
+  let ecra = document.querySelector(".ecra");
+  ecra.innerHTML = "";
+  saida.innerHTML = "";
+}
+
+
+
+function converter(){
+  //Variáveis
+  let ecra = document.querySelector(".ecra");
+  let saida = document.querySelector("#saida");
+
+  //Executar
+  let resultado = parseFloat(ecra.innerHTML) * 0.041;
+  saida.innerHTML = resultado.toFixed(2) + "Kč.";
+}
